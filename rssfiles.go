be5f6625747dfba944a0a0ac171e6b03dbb03a66
spiffytech <spiffytech@gmail.com>
2013-04-03 23:33:10 -0400

    Look, it auths with ge.tt!

--- rssfiles.go
+++ rssfiles.go
@@ -1,16 +1,44 @@
 package main
 
 import (
+    "bytes"
+    "encoding/json"
     "fmt"
     "io"
     "io/ioutil"
     "net/http"
-    //"os"
+    //"net/url"
+    "os"
 
     "github.com/stretchrcom/goweb/goweb"
 )
 
 func main() {
+    type login struct {
+        Apikey string `json:"apikey"`
+        Email string `json:"email"`
+        Password string `json:"password"`
+    }
+    j := login{Apikey: os.Getenv("apikey"), Email: os.Getenv("email"), Password: os.Getenv("password")}
+
+    b, err := json.Marshal(j)
+    if err != nil {
+        panic(err)
+    }
+    reader := bytes.NewReader(b)
+
+    resp, err := http.Post("https://open.ge.tt/1/users/login", "application/json", reader)
+    if err != nil {
+        panic(err)
+    }
+
+    bytes, err := ioutil.ReadAll(resp.Body)
+    if err != nil {
+        panic(err)
+    }
+    fmt.Printf("%s", bytes)
+
+
     goweb.MapFunc("/", func(c *goweb.Context) {
         resp, err := http.Get("http://almamater.xkcd.com/")
         if err != nil {
@@ -23,19 +51,5 @@ func main() {
         ioutil.ReadAll(teeReader)
     })
 
-    goweb.ListenAndServe("0.0.0.0:3000")
-
-    //bytes := make([]byte, 256)
-    //for {
-    //    n, err := resp.Body.Read(bytes)
-    //    if err != nil && n != 0{
-    //        panic(err)
-    //    }
-    //    fmt.Printf("%s", bytes)
-    //    if n == 0 {
-    //        fmt.Println("leaving loop...")
-    //        break
-    //    }
-    //}
-    fmt.Println("here")
+    //goweb.ListenAndServe("0.0.0.0:3000")
 }
