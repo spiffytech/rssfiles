1751956b131613c6891a70d96dc1ac08cc177847
spiffytech <spiffytech@gmail.com>
2013-04-04 09:10:29 -0400

    Now retrieves a list of all files in all shares

--- rssfiles.go
+++ rssfiles.go
@@ -7,11 +7,13 @@ import (
     "io"
     "io/ioutil"
     "net/http"
-    //"net/url"
+    "net/url"
     "os"
 
     "github.com/stretchrcom/goweb/goweb"
 )
+
+var baseurl = "https://open.ge.tt/1/"
 
 func main() {
     type login struct {
@@ -36,8 +38,55 @@ func main() {
     if err != nil {
         panic(err)
     }
-    fmt.Printf("%s", bytes)
 
+    type AuthStuff struct {
+        Accesstoken string
+        Refreshtoken string
+    }
+
+    var authStuff AuthStuff
+    json.Unmarshal(bytes, &authStuff)
+
+    u, err := url.Parse(baseurl)
+    if err != nil {
+        panic(err)
+    }
+    u.Path += "shares"
+    q := u.Query()
+    q.Set("accesstoken", authStuff.Accesstoken)
+    u.RawQuery = q.Encode()
+    fmt.Println(u)
+
+    resp, err = http.Get(u.String())
+    if err != nil {
+        panic(err)
+    }
+    defer resp.Body.Close()
+    body, err := ioutil.ReadAll(resp.Body)
+    if err != nil {
+        panic(err)
+    }
+    fmt.Println(string(body))
+
+    type File struct {
+        Fileid int
+        Filename string
+        Getturl string
+        Created int
+    }
+
+    type Share struct {
+        Sharename string
+        Title string
+        Created int
+        Files []File
+    }
+    var shares []Share
+    json.Unmarshal(body, &shares)
+    fmt.Println(shares)
+}
+
+func runserver() {
 
     goweb.MapFunc("/", func(c *goweb.Context) {
         resp, err := http.Get("http://almamater.xkcd.com/")
